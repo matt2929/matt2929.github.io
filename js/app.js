f89c47b187aca912f7dceb5bb78106ded4328372
matt2929 <mcstaffo@buffalo.edu>
2017-11-22 23:11:57 -0500

    particles

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@
 /* ----------------------------------------------- */
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-
+/*
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('particles.js loaded - callback');
 });
-
+*/
 
 /* Otherwise just put the config content (json): */
 
 particlesJS('particles-js',
-
+  
   {
     "particles": {
       "number": {
